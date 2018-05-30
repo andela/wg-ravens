b36ed1c6d7ebdc8618c828872b565587a40c87dd
Kelvin Kariuki <kelvin.kariuki@andela.com>
2018-05-30 03:20:42 +0300

    [Feature #157589638] restoring previous configs

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn wger.wsgi:application --log-file -
+release: invoke bootstrap-wger --settings-path ./settings.py --no-start-server
--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from wger.settings_global import *
+import django_heroku
 import dj_database_url
 
 # Use 'DEBUG = True' to get more details for server errors
@@ -12,7 +13,6 @@ ADMINS = (
     ('Your name', 'your_email@example.com'),
 )
 MANAGERS = ADMINS
-
 
 DATABASES = {
     'default': {
@@ -29,7 +29,7 @@ if os.environ.get("TRIGGER") == 'TRUE':
     DATABASES['default'] = dj_database_url.config()
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ')z@01nzz5v$f=%tn28*jrjqzb0u-_28f)tyqb-c#%&c-phzdjp'
+SECRET_KEY = 'wv^6)z6)5+5=im=c%u13cd100dfm&4+m^^fu_v96yxd-bl--=b'
 
 # Your reCaptcha keys
 RECAPTCHA_PUBLIC_KEY = ''
@@ -62,13 +62,7 @@ WGER_SETTINGS['EMAIL_FROM'] = 'wger Workout Manager <wger@example.com>'
 # Your twitter handle, if you have one for this instance.
 #WGER_SETTINGS['TWITTER'] = ''
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'wger', 'core', 'static'),
-)
+django_heroku.settings(locals())
+
 
