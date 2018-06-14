fc723bf2068acf1124f5a59524ea21f3000e4313
John Mutuma <johnmutuma5@gmail.com>
2018-06-14 06:48:32 +0300

    [Feature #157589642] Add accounts limit option and adjust message style

--- wger/core/management/commands/add_user_rest_api.py
+++ wger/core/management/commands/add_user_rest_api.py
@@ -13,9 +13,14 @@ class Command(BaseCommand):
                             default=True,
                             help='Grant api consumers right to create users via REST API')
 
+        parser.add_argument('--accounts-limit',
+                            action='store',
+                            dest='limit',
+                            default=3,
+                            help='Limits the number of accounts the API consumer can create per minute')
+
     def handle(self, *args, **options):
         username = options['username'][0]
-        enabled = False
         try:
             target_profile = UserProfile.objects.get(user__username=username)
         except UserProfile.DoesNotExist:
@@ -23,12 +28,22 @@ class Command(BaseCommand):
         # update enabled using command options
         if options['enabled'] in (True, 'True', 'true', 'T', 't', '1', 1):
             enabled = True
+        elif options['enabled'] in (False, 'False', 'false', 'F', 'f', '0', 0):
+            enabled = False
+        else:
+            raise CommandError('Unknown value for option --enabled')
 
         if enabled:
             target_profile.api_add_user_enabled = True
+            try:
+                limit = int(options['limit'])
+                target_profile.api_user_throughput_limit_per_min = limit
+            except ValueError:
+                raise CommandError('Invalid value for --accounts-limit option')
+
             target_profile.save()
             self.stdout.write(self.style.SUCCESS('Successfully ENABLED REST API user creation for "%s"' % username))
         else:
             target_profile.api_add_user_enabled = False
             target_profile.save()
-            self.stdout.write(self.style.SUCCESS('Successfully DISABLED REST API user creation for "%s"' % username))
+            self.stdout.write(self.style.WARNING('Successfully DISABLED REST API user creation for "%s"' % username))
